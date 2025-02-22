79bd025aa3b235d9a68dedbb5dd503aeafdd87c7
Stanislav Chyrva <alkefirko@gmail.com>
2025-02-22 23:47:50 +0200

    add main.ts with responsive canvas setup

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    
-  </div>
-`
+let canvas = document.getElementById("canvas") as HTMLCanvasElement;
+let ctx = canvas.getContext("2d")!;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
